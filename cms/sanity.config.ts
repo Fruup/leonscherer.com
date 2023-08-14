1409698ba83535e5988aea9bb4334a017850c879
Leon Scherer <info@leonscherer.com>
2023-08-15 00:47:21 +0200

    add sanity workspaces

--- cms/sanity.config.ts
+++ cms/sanity.config.ts
@@ -3,16 +3,29 @@ import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas'
 
-export default defineConfig({
-	name: 'default',
+const shared = {
 	title: 'leonscherer.com',
-
 	projectId: 'sg4gqb2a',
-	dataset: 'develop',
 
 	plugins: [deskTool(), visionTool()],
-
 	schema: {
 		types: schemaTypes,
 	},
-})
+}
+
+export default defineConfig([
+	{
+		...shared,
+		title: 'Develop',
+		name: 'develop',
+		basePath: '/develop',
+		dataset: 'develop',
+	},
+	{
+		...shared,
+		title: 'Production',
+		name: 'production',
+		basePath: '/production',
+		dataset: 'production',
+	},
+])
